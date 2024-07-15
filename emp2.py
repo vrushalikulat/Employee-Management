fdd7b2a8b0da95a2cbc17da4c56e01cd2a6f0389
vrushalikulat <124276607+vrushalikulat@users.noreply.github.com>
2024-07-15 13:04:39 +0530

    Update emp2.py

--- emp2.py
+++ emp2.py
@@ -2,7 +2,7 @@
 # A number is even if division by 2 gives a remainder of 0.
 # If the remainder is 1, it is an odd number.
 
-num = int(input("Enter a number: "))
+num = int(input("Enter number"))
 if (num % 2) == 0:
    print("{0} is Even".format(num))
 else:
