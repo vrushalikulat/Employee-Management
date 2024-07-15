d42cea462bda2bf0734da405a46550793e1d4f80
vrushalikulat <124276607+vrushalikulat@users.noreply.github.com>
2024-07-15 13:30:35 +0530

    Update emp1.py
    result line updated

--- emp1.py
+++ emp1.py
@@ -6,4 +6,4 @@ num2 = input('Enter second number')
 sum = float(num1) + float(num2)
 
 # Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('The addition of {0} and {1} is {2}'.format(num1, num2, sum))
